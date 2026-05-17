f5b6eeee7702b6c348bff20ff7f12bade2be29d5
joabeplay1 <joabejonatas186@gmail.com>
2026-05-17 15:30:17 -0300

    firebase-config.js
    Added Firebase configuration for the project.

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase do seu Projeto
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY_AQUI",
+    authDomain: "SEU_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://SEU_PROJECT_ID-default-rtdb.firebaseio.com",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_PROJECT_ID.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SUA_APP_ID"
+};
+
+export default firebaseConfig;
