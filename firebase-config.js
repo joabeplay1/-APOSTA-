e727d0dd81f3e4abcd6ae05df6b1ea4a93fd26ce
joabeplay1 <joabejonatas186@gmail.com>
2026-05-18 02:03:58 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-// Configuração do Firebase do seu Projeto
+// Configuração Oficial do Firebase — Domino Aposta
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "SEU_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://SEU_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_PROJECT_ID.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SUA_APP_ID"
+    apiKey: "AIzaSyCZ4rOliexofYP8vyRLzUeX3mf5uXG6WRM",
+    authDomain: "aposta-96213.firebaseapp.com",
+    databaseURL: "https://aposta-96213-default-rtdb.firebaseio.com",
+    projectId: "aposta-96213",
+    storageBucket: "aposta-96213.firebasestorage.app",
+    messagingSenderId: "989060185373",
+    appId: "1:989060185373:web:69bb80b2f961fe8e9d35f4",
+    measurementId: "G-1LTXVCXHX5"
 };
 
 export default firebaseConfig;
