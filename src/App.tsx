33b449d5e002c8e12f1687035a9672924536c408
joabeplay1 <joabejonatas186@gmail.com>
2026-07-08 17:21:36 -0300

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App(){
+
+  return (
+
+    <div className="app">
+
+      <h1>
+        BuildCloud
+      </h1>
+
+
+      <p>
+        Plataforma de compilação usando GitHub Actions
+      </p>
+
+
+      <button>
+
+        Entrar com GitHub
+
+      </button>
+
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
