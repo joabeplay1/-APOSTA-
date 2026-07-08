87996bd7c4fea71a01b162dac9693d5c343de78c
joabeplay1 <joabejonatas186@gmail.com>
2026-07-08 17:18:47 -0300

    main.ts

---
+++ electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+
+  const window = new BrowserWindow({
+    width: 1400,
+    height: 900,
+
+    webPreferences: {
+      preload: path.join(
+        __dirname,
+        "preload.js"
+      )
+    }
+  });
+
+
+  window.loadURL(
+    "http://localhost:5173"
+  );
+
+}
+
+
+app.whenReady()
+.then(() => {
+
+  createWindow();
+
+});
